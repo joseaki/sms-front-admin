8e99cc2ae51d0382bf5420991fed0ed46db187a3
Antonio Alanya <jose.alanya@trade.ec>
2021-12-08 14:16:56 -0500

    test netlify rollback

--- src/Presentation/users/Components/searchBar.tsx
+++ src/Presentation/users/Components/searchBar.tsx
@@ -35,7 +35,7 @@ const SearchBar = () => {
               inputRef={register()}
               type="text"
               fullWidth
-              label="Nombre de usuario"
+              label="Nombre de usuario nuevo test"
               name="query"
               id="query"
             />
